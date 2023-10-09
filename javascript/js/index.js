c962f17aec3129e5085a22f9c080b79b6e58b028
CrisGadea <cristianhernangadea@gmail.com>
2023-10-09 20:18:21 -0300

    funciones

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -65,7 +65,7 @@ console.log(10.0 === 10)// 50 + 30 = 80
 // sino, le pediremos que ingrese cuando sea mayor de edad.
 
 // preguntar la edad
-
+/*
 var edad = prompt("Ingrese su edad")
 
 console.log(edad)
@@ -103,9 +103,91 @@ switch(letra){
     default:
         console.log("Ingrese unicamente caracter letra");
         break;
+}*/
+
+// Ejercicio: Realizar una tabla de multiplicar dado un número por pantalla
+// obteniendo el resultado por consola del navegador
+
+//var numero = parseInt(prompt("Ingrese un número para conocer su tabla de múltiplicar (hasta 10)"))
+
+/* for (let i = 0; i < 10; i++) {
+    console.log(numero + " * " + i + " = " + (numero * i))
+} */
+
+// Ejercicio 2: En base a la tabla de multiplicar de arriba, mostrar únicamente
+// los resultados pares
+/* for (let i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+        console.log(numero + " * " + i + " = " + (numero * i))  
+    }
+} */
+
+// Bucle WHILE
+
+/* var opcion = Number(prompt("1. Ver hora actual \n0. Salir"));
+
+
+while (opcion != 0) {
+    switch(opcion){
+        case 1:
+            alert(Date());
+            break;
+        case 0:
+            console.log("Exit");
+            break;
+        default:
+            console.log("Elija una opción correcta");
+            break;
+    }
+        opcion = Number(prompt("1. Ver hora actual \n0. Salir"));    
+} */
+
+// DO WHILE
+/* do{
+
+    var opcion = Number(prompt("1. Ver hora actual \n0. Salir"));
+
+    switch(opcion){
+        case 1:
+            alert(Date());
+            break;
+        case 0:
+            console.log("Exit");
+            break;
+        default:
+            console.log("Elija una opción correcta");
+            break;
+    }
+
+} while(opcion !== 0) */
+
+
+
+// FUNCIONES
+// Son bloques de código que se ejecutarán cuando se llamen
+
+console.log("ANTES DE LA LLAMADA");
+
+saludar();
+
+var num1 = Number(prompt("Ingrese un número"));
+var num2 = Number(prompt("Ingrese otro número"));
+
+console.log("El resultado de sumar ambos números es: " + sumar(num1,num2));
+
+console.log("DESPUES DE LA LLAMADA");
+
+console.log("Suma final: " + sumar(sumar(12,12), 14));
+
+function saludar(){
+    alert("HELLO WORLD!");
+    console.log("Estoy saludando");
 }
 
+// CREAR LAS OPERACIONES BASICAS Y SOLICITAR AL USUARIO LOS VALORES
+// Y LA OPCION A EJECUTAR
 
-
-
+function sumar(num1, num2) {
+    return num1 + num2;
+}
 
